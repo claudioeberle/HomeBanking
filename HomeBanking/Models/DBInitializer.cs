0428ec0ef2da3c0eadf3fd21bcb4e2709b2cb48d
claudioeberle <claudioeberle@gmail.com>
2023-07-24 15:20:23 -0300

    Add project files.

---
+++ HomeBanking/Models/DBInitializer.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+
+namespace HomeBanking.Models
+{
+    public class DBInitializer
+    {
+        public static void Initialize(HomeBankingContext context)
+        {
+            if (!context.Clients.Any())
+            {
+                //creamos datos de prueba
+
+                Client[] clients = new Client[]
+                {
+                    new Client { Email = "vcoronado@gmail.com", FirstName="Victor", LastName="Coronado", Password="123456"}
+                };
+
+                foreach (Client client in clients)
+                {
+                    context.Clients.Add(client);
+                }
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
