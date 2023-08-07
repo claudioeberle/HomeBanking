ccf2846beff8d5f269cfbca9e183b1673e12b023
claudioeberle <claudioeberle@gmail.com>
2023-08-07 09:51:26 -0300

    added web resources httpGetCurrent, httpPost @ClientController AuthController
    created
    new Client "Claudio Eberle" from index.html

--- HomeBanking/Controllers/ClientsController.cs
+++ HomeBanking/Controllers/ClientsController.cs
@@ -78,7 +78,6 @@ namespace HomeBanking.Controllers
         }
 
         [HttpGet("{id}")]
-
         public IActionResult Get(long id) 
         {
             try
@@ -138,6 +137,106 @@ namespace HomeBanking.Controllers
                 };
 
                 return Ok(clientDTO);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpGet("current")]
+        public IActionResult GetCurrent()
+        {
+            try
+            {
+                string email = User.FindFirst("Client") != null ? User.FindFirst("Client").Value : String.Empty;
+                if(email == String.Empty) 
+                {
+                    return Forbid();
+                }
+
+                Client client = _clientRepository.FindByEmail(email);
+
+                if(client == null)
+                {
+                    return Forbid();
+                }
+
+                var clientDTO = new ClientDTO
+                {
+                    Id = client.Id,
+                    Email = client.Email,
+                    FirstName = client.FirstName,
+                    LastName = client.LastName,
+                    Accounts = client.Accounts.Select(ac => new AccountDTO
+                    {
+                        Id = ac.Id,
+                        Balance = ac.Balance,
+                        CreationDate = ac.CreationDate,
+                        Number = ac.Number
+                    }).ToList(),
+                    Credits = client.ClientLoans.Select(cl => new ClientLoanDTO
+                    {
+                        Id = cl.Id,
+                        LoanId = cl.LoanId,
+                        Name = cl.Loan.Name,
+                        Amount = cl.Amount,
+                        Payments = int.Parse(cl.Payments)
+                    }).ToList(),
+                    Cards = client.Cards.Select(c => new CardDTO
+                    {
+                        Id = c.Id,
+                        CardHolder = c.CardHolder,
+                        Color = c.Color,
+                        Cvv = c.Cvv,
+                        FromDate = c.FromDate,
+                        Number = c.Number,
+                        ThruDate = c.ThruDate,
+                        Type = c.Type
+                    }).ToList()
+                };
+
+                return Ok(clientDTO);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Client client) 
+        {
+            try
+            {
+                //validate data
+                if(String.IsNullOrEmpty(client.Email) ||
+                    String.IsNullOrEmpty(client.Password) ||
+                    String.IsNullOrEmpty(client.FirstName) ||
+                    String.IsNullOrEmpty(client.LastName))
+                {
+                    return StatusCode(403, "Datos Inválidos");
+                }
+
+                //look for existing user
+                Client user = _clientRepository.FindByEmail(client.Email);
+
+                if (user != null)
+                {
+                    return StatusCode(403, "El Email ya está en uso");
+                }
+
+                Client newClient = new Client
+                {
+                    Email = client.Email,
+                    Password = client.Password,
+                    FirstName = client.FirstName,
+                    LastName = client.LastName,
+                };
+
+                _clientRepository.Save(newClient);
+                return Created("", newClient);
+
             }
             catch (Exception ex)
             {
